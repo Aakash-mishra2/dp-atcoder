c9481247e4b3ec289ed329ad23235ac208b9d2f3
SKY <95024991+Aakash-mishra2@users.noreply.github.com>
2023-10-23 20:46:05 +0530

    Update dp-vacation.cpp

--- dp-vacation.cpp
+++ dp-vacation.cpp
@@ -13,27 +13,30 @@ void file_i_o(){
 	freopen("input.txt", r, stdin);
 }
 
+int happinessDP(int n, vector<vector<int>> points ){
+    vector<vector<int>> dp(n, vector<int>(3, 0));
+    int curr = 0;
+    dp[0][0] = points[0][0];
+    dp[0][1] = points[0][1];
+    dp[0][2] = points[0][2];
+    for(int i = 1; i<n; i++){
+        dp[i][0] = points[i][0] + max(dp[i-1][1], dp[i-1][2]);
+        dp[i][1] = points[i][1] + max(dp[i-1][0], dp[i-1][2]);
+        dp[i][2] = points[i][2] + max(dp[i-1][0], dp[i-1][1]);
+    }
+    return max({dp[n-1][0], dp[n-1][1], dp[n-1][2]});
+}
+
 int main(){
 	file_i_o();
-
-	ll n;
-	cin>>n;
-	vector<vector<ll>> dp(n, vector<ll>(3, 0));
-
-	ll a, b, c;
-	cin>>a>>b>>c;
-	//initialize case
-	dp[0][0] = a;
-	dp[0][1] = b;
-	dp[0][2] = c;
-
-	loop(i, 1, n-1){
-	if(i - 2 < 0) break;
-
-	dp[i][2] = (c + max(dp[i-1][0], dp[i-1][1])); 
-	dp[i][0] = (a + max(dp[i-1][1], dp[i-1][2]));
-	dp[i][1] = (b + max(dp[i-1][2], dp[i-1][0]));
-	}
-	cout<< max({ dp[n-1][0], dp[n-1][1], dp[n-1][2] });
+	int n; cin>>n;
+    vector<vector<int>> points(n, vector<int> (3, 0));
+    for(int j = 0; j<n; j++)
+    for( int i = 0; i<3; i++){
+        cin>>points[j][i];
+    }
+    int ans = happinessDP(n, points);
+    cout<<ans<<endl;
+    return 0;
 	
 }
